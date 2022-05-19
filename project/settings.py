05873f9ac07ff3d8f4b42d753fa8780563909e7c
Sujan Adhikari <sujanadhikari.com@gmail.com>
2022-05-20 03:37:17 +0545

    final commit 1

--- project/settings.py
+++ project/settings.py
@@ -129,6 +129,11 @@ STATICFILES_DIRS = ['project/static']
 
 MEDIA_URL   = '/media/'
 
+
+CSRF_TRUSTED_ORIGINS = [
+    'https://hemant-to-do.herokuapp.com'
+]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
