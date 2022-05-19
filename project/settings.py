1b8525446fe1fca7990bf5599dac29331dadd197
Sujan Adhikari <sujanadhikari.com@gmail.com>
2022-05-20 03:56:24 +0545

    final commit 3

--- project/settings.py
+++ project/settings.py
@@ -131,7 +131,7 @@ MEDIA_URL   = '/media/'
 
 
 CSRF_TRUSTED_ORIGINS = [
-    'https://bmi-web-django.herokuapp.com/'
+    'https://bmi-web-django.herokuapp.com'
 ]
 
 # Default primary key field type
